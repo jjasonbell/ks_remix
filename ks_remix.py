3d35ed5f5942ebe19d318d070b06a8bfe16cb6c7
jjasonbell <j.jason.bell@gmail.com>
2019-01-04 13:06:47 +0000

    Working version of ks_remix.  Not horrible.

--- ks_remix.py
+++ ks_remix.py
@@ -1,6 +1,18 @@
 import pandas as pd
-import os
+import os, random
 os.chdir(os.getenv('HOME') + '/Documents/Novaline/ks_remix')
 
 
 df = pd.read_csv('ks_remix.csv')
+with open('targets.txt', 'r', encoding='utf-8') as f:
+	targets = f.read()
+targets = targets.split("\n")
+
+names = df.name
+
+num = 10
+for i in range(0, num):
+	name = random.choice(names)
+	target = random.choice(targets)
+	print(name, 'FOR', target)
+	print('\n')
