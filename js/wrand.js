7682d94afa9808dc45d06f22b5385846f1e22546
jjasonbell <j.jason.bell@gmail.com>
2019-01-05 21:04:17 +0000

    Pure javascript implementation, hardcoded data

--- js/wrand.js
+++ js/wrand.js
@@ -1,10 +1,10 @@
-function weightedRand(ycarray) {
+function weightedRand(ksarray) {
   var i; 
   var sum = 0; 
   r = Math.random();
-	var arrayLength = ycarray.length;
+	var arrayLength = ksarray.length;
 	for (var i = 0; i < arrayLength; i++) {
-    json_i = ycarray[i];
+    json_i = ksarray[i];
     sum += json_i['Weight'];
     if (r <= sum) return json_i;
   }
